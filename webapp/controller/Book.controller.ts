fb1bf6fcc053faf13f525fd5828d8307483b943f
Kliment Simonchev <kliment.simonchev@sapiens.eu>
2024-02-12 14:20:34 +0000

    Local JSON Model

--- webapp/controller/Book.controller.ts
+++ webapp/controller/Book.controller.ts
@@ -7,6 +7,7 @@ import Binding from "sap/ui/model/Binding";
 import Filter from "sap/ui/model/Filter";
 import FilterOperator from "sap/ui/model/FilterOperator";
 import FilterType from "sap/ui/model/FilterType";
+import JSONModel from "sap/ui/model/json/JSONModel";
 
 
 
@@ -18,6 +19,11 @@ export default class Book extends Controller {
 
     /*eslint-disable @typescript-eslint/no-empty-function*/
     public onInit(): void {
+        var oView: View  = this.getView() as View;
+        let data = {ui: {download:true}}
+        let guiModel = new JSONModel(data)
+        oView.setModel(guiModel,"bookModel")
+
 
     }
     public onSearch():void{
@@ -30,5 +36,11 @@ export default class Book extends Controller {
         let bind = table.getBinding("items") as any
 
 		bind.filter(oFilter, FilterType.Application);
+    }
+    public checkDownload(event: any):void {
+        var bSelected = event.getParameter('selected');
+        var oView: View  = this.getView() as View;
+       
+        oView.getModel()?.getProperty("ui/download").set(bSelected)
     }
 }
